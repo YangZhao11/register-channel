13bc47a9dfc82142b16f8d96748f8221b78e3d81
YangZhao11 <YangZhao11@users.noreply.github.com>
2014-08-01 13:58:04 -0700

    Make register-channel a global minor mode; bind keys through a mode keymap.

--- register-channel.el
+++ register-channel.el
@@ -1,14 +1,21 @@
 ;;; register-channel.el --- Jump around fast using registers
-;;
+
+;; Copyright 2011 Google Inc. All Rights Reserved.
+
+;; Author: Yang Zhao <zhyang@google.com>
+;; Maintainer: Yang Zhao <zhyang@google.com>
+;; Created: Mar 29 2011
+;; Version: 0.1
+
 ;; To use, put this in your init file:
 ;; (require 'register-channel)
-;; (register-channel-default-keybindings)
+;; (register-channel-mode 1)
 ;;
-;; Then you can use M-g M-1 to put point position into register
-;; 1. This works for register 1 to 5; 6 to 8 by default hold window
-;; configurations. To utilize these registers, use M-1 etc. The old
-;; position / window configuration are automatically stored into
-;; register `, so you can easily go back with M-`.
+;; Then you can use ESC ESC 1 to put point position into register
+;; 1. This works for register 1 to 5; 6 and 7 by default holds texts,
+;; 8 and 9 holds window configurations. To utilize these registers,
+;; use M-1 etc. The old position / window configuration are
+;; automatically stored into register 0, so you can easily go back.
 
 (require 'register)
 
@@ -23,7 +30,15 @@
     (set-register register-channel-backup-register register-val)
     (setq register-channel-last-save-type type)))
 
-(defun jump-or-insert-self-register (&optional arg)
+(defun register-channel-last-command-char ()
+  "Returns the character corresponding to last command, stripping
+  any modifiers. E.g. if last command is M-1, should return 1."
+  (let ((char (if (integerp last-command-event)
+                   last-command-event
+                 (get last-command-event 'ascii-character))))
+    (logand char ?\177)))
+
+(defun register-channel-jump-or-insert (&optional arg)
   "Utilize the register pressed with the last key. If the command
 is invoked with M-1, then the last key is `1'.
 
@@ -36,10 +51,7 @@ The replaced content is saved in register
 can jump back easily."
   (interactive "P")
   ;; Refer to code in digit-argument and register.el.
-  (let* ((char (if (integerp last-command-event)
-                   last-command-event
-                 (get last-command-event 'ascii-character)))
-         (register (logand char ?\177))
+  (let* ((register (register-channel-last-command-char))
          (val (get-register register)))
     (cond ((not val)
            (message "No content in register %c" register))
@@ -69,25 +81,19 @@ can jump back easily."
           ('t
            (insert-register register arg)))))
 
-(defun point-to-self-register (&optional arg)
+(defun register-channel-save-point (&optional arg)
   "Save point to register defined by last key press. E.g. if this
 function is bound to ESC M-1, the point is saved in register 1."
   (interactive "P")
-  (let* ((char (if (integerp last-command-event)
-                   last-command-event
-                 (get last-command-event 'ascii-character)))
-         (digit-char (logand char ?\177)))
+  (let ((digit-char (register-channel-last-command-char)))
     (point-to-register digit-char)
     (set-marker-insertion-type (get-register digit-char) arg)
     (message "Point stored in register %c" digit-char)))
 
-(defun copy-to-self-register (&optional delete-flag)
+(defun register-channel-save-text (&optional delete-flag)
   "Copy region to register defined by last key press."
   (interactive "P")
-  (let* ((char (if (integerp last-command-event)
-                   last-command-event
-                 (get last-command-event 'ascii-character)))
-         (digit-char (logand char ?\177)))
+  (let ((digit-char (register-channel-last-command-char)))
     (copy-to-register digit-char
                       (region-beginning) (region-end) delete-flag)
     (let* ((killed-text (get-register digit-char))
@@ -98,48 +104,48 @@ function is bound to ESC M-1, the point is saved in register 1."
         (message "Saved text from \"%s\" to register %c"
                  (substring killed-text 0 message-len) digit-char)))))
 
-(defun window-configuration-to-self-register (&optional arg)
+(defun register-channel-save-window-configuration (&optional arg)
   "Save window configuration to register defined by last key press."
   (interactive "P")
-  (let* ((char (if (integerp last-command-event)
-                   last-command-event
-                 (get last-command-event 'ascii-character)))
-         (digit-char (logand char ?\177)))
+  (let ((digit-char (register-channel-last-command-char)))
     (window-configuration-to-register digit-char arg)
     (message "Window configuration saved in register %c" digit-char)))
 
-(defun frame-configuration-to-self-register (&optional arg)
+(defun register-channel-save-frame-configuration (&optional arg)
   "Save frame configuration to register defined by last key press."
   (interactive "P")
-  (let* ((char (if (integerp last-command-event)
-                   last-command-event
-                 (get last-command-event 'ascii-character)))
-         (digit-char (logand char ?\177)))
+  (let ((digit-char (register-channel-last-command-char)))
     (frame-configuration-to-register digit-char arg)
     (message "Frame configuration saved in register %c" digit-char)))
 
+(defun register-channel-default-keymap ()
+  (let ((map (make-sparse-keymap)))
+  (define-key map (kbd "M-g M-1") 'register-channel-save-point)
+  (define-key map (kbd "M-g M-2") 'register-channel-save-point)
+  (define-key map (kbd "M-g M-3") 'register-channel-save-point)
+  (define-key map (kbd "M-g M-4") 'register-channel-save-point)
+  (define-key map (kbd "M-g M-5") 'register-channel-save-point)
+  (define-key map (kbd "M-g M-6") 'register-channel-save-window-configuration)
+  (define-key map (kbd "M-g M-7") 'register-channel-save-window-configuration)
+  (define-key map (kbd "M-g M-8") 'register-channel-save-window-configuration)
+  (define-key map (kbd "M-`") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-1") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-2") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-3") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-4") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-5") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-6") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-7") 'register-channel-jump-or-insert)
+  (define-key map (kbd "M-8") 'register-channel-jump-or-insert)
+  map))
+
+(defvar register-channel-mode-map (register-channel-default-keymap)
+  "Key map for register-channel minor mode")
+
 ;;;###autoload
-(defun register-channel-default-keybindings ()
-  "Default key bindings for register-channel: ESC M-1 to 5 to
-save point, ESC M-6 to 8 to save window configuration. M-1 to 8
-to use the saved register."
-  (interactive)
-  (global-set-key (kbd "M-g M-1") 'point-to-self-register)
-  (global-set-key (kbd "M-g M-2") 'point-to-self-register)
-  (global-set-key (kbd "M-g M-3") 'point-to-self-register)
-  (global-set-key (kbd "M-g M-4") 'point-to-self-register)
-  (global-set-key (kbd "M-g M-5") 'point-to-self-register)
-  (global-set-key (kbd "M-g M-6") 'window-configuration-to-self-register)
-  (global-set-key (kbd "M-g M-7") 'window-configuration-to-self-register)
-  (global-set-key (kbd "M-g M-8") 'window-configuration-to-self-register)
-  (global-set-key (kbd "M-`") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-1") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-2") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-3") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-4") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-5") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-6") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-7") 'jump-or-insert-self-register)
-  (global-set-key (kbd "M-8") 'jump-or-insert-self-register))
+(define-minor-mode register-channel-mode
+  "Toggle register-channel mode"
+  :keymap register-channel-mode-map
+  :global t)
 
 (provide 'register-channel)
