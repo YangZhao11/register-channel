234c59bdfa134dd6967d75f6df6841cdba77134b
YangZhao11 <YangZhao11@users.noreply.github.com>
2014-08-01 19:15:21 -0700

    Update comment.

--- register-channel.el
+++ register-channel.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2014  Yang Zhao
 
-;; Author: Yang Zhao <YangZhao11users.noreply.github.com>
+;; Author: Yang Zhao <YangZhao11@users.noreply.github.com>
 ;; Keywords: convenience
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -25,7 +25,16 @@
 
 ;;; Commentary:
 
-;;
+;; This package provides key bindings to quickly switch between
+;; predefined places, and move text to these places. With the default
+;; binding, use M-g 1 to store current point to register 1, use M-1 to
+;; go to it later; this works for 1~5. With an active region, M-1 will
+;; copy text to the place stored in register 1. 6~8 stores / recalls
+;; window configuration by default.
+
+;; To use, put into your init.el:
+;; (require 'register-channel)
+;; (register-channel-mode 1)
 
 ;;; Code:
 
